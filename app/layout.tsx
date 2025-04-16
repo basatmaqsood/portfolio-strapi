61aace38a7c677de5c7016d308fdc2c6e92db6dd
basat <itzbasatmaqsood@gmail.com>
2025-04-16 22:21:30 +0500

    trying to deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,10 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import MainLayout from "@/components/layout/main-layout";
-import CustomCursor from "@/components/custom-cursor";
 import AnimatedBackground from "@/components/animated-background";
 import ScrollProgress from "@/components/scroll-progress";
 import PageTransition from "@/components/page-transition";
 import GrainOverlay from "@/components/grain-overlay";
-import AnimatedCursor from "react-animated-cursor";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -35,43 +33,15 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} font-poppins bg-black text-white min-h-screen`}
       >
-        <AnimatedBackground />
+        {/* <AnimatedBackground /> */}
         {/* <CustomCursor />
          */}
-<AnimatedCursor
 
-clickables={[
-  'a',
-  'input[type="text"]',
-  'input[type="email"]',
-  'input[type="number"]',
-  'input[type="submit"]',
-  'input[type="image"]',
-  'label[for]',
-  'select',
-  'textarea',
-  'button',
-  '.link',
-]}
-  innerSize={8}
-  color='193, 11, 111'
-  outerSize={35}
-  innerScale={1}
-  outerScale={2}
-  outerAlpha={0}
-  trailingSpeed={1}
-  innerStyle={{
-    backgroundColor: '#fff'
-  }}
-  outerStyle={{
-    border: '3px solid #fff'
-  }}
-/>
         <ScrollProgress />
         <GrainOverlay />
-        <PageTransition>
+        {/* <PageTransition> */}
           <MainLayout>{children}</MainLayout>
-        </PageTransition>
+        {/* </PageTransition> */}
       </body>
     </html>
   );
